90cefdedd298dab8f6b4e0fa1af83a4a4aff1ebe
mbuziak <marek.buziak@gmail.com>
2020-06-19 08:54:42 +0200

    Przesienie /partials/ do /assets/, style, struktura navbar + sticky

--- dist/js/index.babel.js
+++ dist/js/index.babel.js
@@ -2,8 +2,31 @@
 
 window.addEventListener('DOMContentLoaded', function () {
   console.log('DOM fully loaded and parsed');
-  console.log('test - skrypt');
-  var observer = lozad(); // lazy loads elements with default selector as '.lozad'
+  console.log('test - skrypt'); // ### user functions start ###
+  // ### user functions end ###
+  // ### plugins/implementations start ###
 
-  observer.observe();
-});
+  toggleNavbar(); // lazy loads elements with default selector as '.lozad'
+
+  var observer = lozad();
+  observer.observe(); // ### plugins/implementations end ###
+}); // toggle navbar and hamburger menu
+
+var toggleNavbar = function toggleNavbar() {
+  // Get all "navbar-burger" elements
+  var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0); // Check if there are any navbar burgers
+
+  if ($navbarBurgers.length > 0) {
+    // Add a click event on each of them
+    $navbarBurgers.forEach(function (el) {
+      el.addEventListener('click', function () {
+        // Get the target from the "data-target" attribute
+        var target = el.dataset.target;
+        var $target = document.getElementById(target); // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+      });
+    });
+  }
+};
--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,75 @@ window.addEventListener('DOMContentLoaded', () => {
 	console.log('DOM fully loaded and parsed');
 	console.log('test - skrypt');
 	
-	const observer = lozad(); // lazy loads elements with default selector as '.lozad'
+	
+	// ### user functions start ###
+	
+	// ### user functions end ###
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	// ### plugins/implementations start ###
+	
+	toggleNavbar();
+	
+	// lazy loads elements with default selector as '.lozad'
+	const observer = lozad(); 
 	observer.observe();
+	
+	// ### plugins/implementations end ###
+	
+	
+	
+	
+	
+	
 
 });
+
+
+// toggle navbar and hamburger menu
+const toggleNavbar = () => {
+	
+	// Get all "navbar-burger" elements
+	const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+	// Check if there are any navbar burgers
+	if ($navbarBurgers.length > 0) {
+
+		// Add a click event on each of them
+		$navbarBurgers.forEach(el => {
+			el.addEventListener('click', () => {
+
+				// Get the target from the "data-target" attribute
+				const target = el.dataset.target;
+				const $target = document.getElementById(target);
+
+				// Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+				el.classList.toggle('is-active');
+				$target.classList.toggle('is-active');
+
+			});
+		});
+	}
+};
+
